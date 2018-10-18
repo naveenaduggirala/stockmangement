88280fa1a7ec51160692cba9a2f72b966798b75b
naveena7 <naveena.d@dhanushinfotech.net>
2018-10-18 15:51:44 +0530

    changes

--- wine_products/views.py
+++ wine_products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Categorie,Qunatite,Product
 from .forms import ProductForm
+from django.shortcuts import render_to_response
+from django.template import Context, Template
 from django.shortcuts import get_list_or_404, get_object_or_404
 # Create your views here.
 
@@ -8,21 +10,31 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 def product_add(request,id=None,product_obj=None,template_name="wine_products/product_add.html"):
 	if id:
 		product_obj = get_object_or_404(Product, pk=id)
-	else:
 		if request.method == 'POST':
 			form = ProductForm(request.POST, instance=product_obj)
-		if form.is_valid():
-			form.save()
-			if not id:
+			if form.is_valid():
+				form.save()
+				messages.success(request, 'Product details updated.')
+			else:
+				print form.errors
+		else:
+			form = ProductForm(instance=product_obj)
+
+	else:
+		print "new form"
+		if request.method == 'POST':
+			form = ProductForm(request.POST, instance=product_obj)
+			if form.is_valid():
+				form.save()
 				messages.success(request, 'New product saved successfully.')
 			else:
-				messages.success(request, 'Product details updated.')
+				print form.errors
 		else:
-			print form.errors
-	else:
-		form = ProductForm(instance=product_obj)
+			form = ProductForm(instance=product_obj)
+
+	
 	return render_to_response(template_name,
-							  RequestContext(request))
+							 Context(request))
 	
 
 
