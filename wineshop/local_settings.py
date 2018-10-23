2d92423b22042dfec909369c12728f5e37af74af
naveenaduggirala <deepikaduggirala7@gmail.com>
2018-10-23 22:40:58 +0530

    login page,home page

---
+++ wineshop/local_settings.py
@@ -0,0 +1,24 @@
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'wineshop',
+
+        'USER': 'pgesu',
+
+        'PASSWORD': 'error',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '',
+
+    }
+
+}
+
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = "/home/chinni/stockmangement/media"
